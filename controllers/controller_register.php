b6bf6feeda89156f0688e3588c3e26588e7eec60
Omccc <138005852+Omccc@users.noreply.github.com>
2023-11-10 16:23:16 +0100

    controller_register.php

--- controllers/controller_register.php
+++ controllers/controller_register.php
@@ -82,31 +82,6 @@ include "./views/layout.phtml";
 
 
 
-// Explications de ce code 
-
-
-    // Ce code gère l'insertion de nouvelles données dans une table de la base de données à partir d'un formulaire. Voici une explication étape par étape du code :
-
-    //     1. **Validation du formulaire** :
-    //        - Le code commence par vérifier si le formulaire a été soumis. Cela est fait en vérifiant si la clé "submit" existe dans le tableau `$_POST`. Si le formulaire a été soumis (c'est-à-dire si l'utilisateur a cliqué sur un bouton "submit" dans le formulaire), le code continue à exécuter la logique d'insertion.
-        
-    //     2. **Connexion à la base de données** :
-    //        - Une connexion à la base de données est établie en utilisant la fonction `connectDB()`. Cela crée un objet PDO qui sera utilisé pour interagir avec la base de données.
-        
-    //     3. **Préparation et exécution de la requête SQL** :
-    //        - Une requête SQL est préparée pour insérer des données dans la table "users". Les valeurs à insérer sont passées en utilisant des paramètres de substitution (`?`) dans la requête, ce qui protège contre les attaques SQL par injection.
-    //        - Les valeurs à insérer sont récupérées à partir du tableau `$_POST`. Il s'agit des valeurs des champs du formulaire "firstName", "name", "mail", et "password".
-    //        - Le mot de passe est haché à l'aide de la fonction `password_hash()` avant d'être inséré dans la base de données. Cela garantit que le mot de passe est stocké de manière sécurisée.
-    //        - La requête est exécutée en utilisant `$sql->execute()`.
-        
-    //     4. **Redirection vers une autre page** :
-    //        - Après avoir inséré les données avec succès, l'utilisateur est redirigé vers la page "adminlist" en utilisant `header("Location:?page=adminlist")`. Cela permet généralement de rediriger l'utilisateur vers une liste d'éléments existants ou de confirmer que les données ont été ajoutées avec succès.
-        
-    //     5. **Inclusion de la vue** :
-    //        - Enfin, le code inclut le fichier de vue "./views/layout.phtml". La vue peut contenir un message de confirmation ou une redirection vers une autre page, mais les détails spécifiques de la vue ne sont pas inclus dans le code fourni.
-        
-    //     En résumé, ce code gère l'insertion de nouvelles données d'utilisateur dans la table "users" de la base de données. Il effectue la validation du formulaire, la connexion à la base de données, l'insertion des données, la redirection de l'utilisateur vers une autre page et l'inclusion de la vue.
-
 
 
 
